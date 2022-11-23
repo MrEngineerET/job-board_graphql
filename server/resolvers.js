3fcbaa81143a1da7a99b2fdc4ee7d6b199fc3851
Biruk Berhanu <meetbirukberhanu@gmail.com>
2022-11-23 10:34:45 +0300

    add resolver for company field

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,12 @@
-import { Job } from "./db.js";
+import { Job, Company } from "./db.js";
 
 export default {
   Query: {
     jobs: () => Job.findAll(),
+  },
+  Jobs: {
+    company: ({ companyId }) => {
+      return Company.findById(companyId);
+    },
   },
 };
